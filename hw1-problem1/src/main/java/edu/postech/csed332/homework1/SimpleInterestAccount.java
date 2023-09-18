fa771bb4784e5bc1be26e272298ad5128b1a11aa
Gwanho <khkim6040@postech.ac.kr>
2023-09-18 20:37:31 +0900

    Feat: Fill in Todos initially

--- hw1-problem1/src/main/java/edu/postech/csed332/homework1/SimpleInterestAccount.java
+++ hw1-problem1/src/main/java/edu/postech/csed332/homework1/SimpleInterestAccount.java
@@ -5,9 +5,14 @@ package edu.postech.csed332.homework1;
  * rate r and the elapsed date d, the new balance is B * (1 + r * d/365).
  */
 class SimpleInterestAccount extends AbstractAccount implements Account {
-    //TODO implement this (including fields and a constructor if needed)
+    public SimpleInterestAccount(int accountNumber, String name, double initial, double rate) {
+        super(accountNumber, name, initial, rate);
+    }
 
     public void elapseTime(int elapsedDate) {
-        //TODO implement this
+        double annualInterestRate = getAnnualInterestRate();
+        double elapsedYear = elapsedDate/(double)365;
+        double result = getBalance()*(1+annualInterestRate*elapsedYear);
+        setBalance(result);
     }
 }
